9f69fa5ff2ec67e0474e2af6bf1ae6a3fbbcf968
Edy Li <gtechoogle@gmail.com>
2024-01-10 00:13:19 +0800

    Use appuim fwk to do auto test
    take first try to use appium to do auto test

--- start.py
+++ start.py
@@ -1,1 +1,17 @@
+from appium import webdriver
+from appium.options.android import UiAutomator2Options
 import os
+
+capabilities = dict(
+    platformName='Android',
+    automationName='uiautomator2',
+    deviceName='Android',
+    app='\\testApk\\news_article.apk'
+)
+
+appium_server_url = 'http://localhost:4723'
+capabilities['app'] = os.path.join(os.getcwd(),"testApk\\news_article.apk")
+print(capabilities)
+
+driver = webdriver.Remote(appium_server_url, options=UiAutomator2Options().load_capabilities(capabilities))
+driver.quit()
