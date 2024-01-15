01f7ebf0893892fe05b457bf8568fb8aa478b841
Edy Li <gtechoogle@gmail.com>
2024-01-15 22:35:27 +0800

    fix some bugs
    change the config structure

--- app_init.py
+++ app_init.py
@@ -1,7 +1,10 @@
 from appium import webdriver
 from appium.options.android import UiAutomator2Options
 from appium.webdriver.common.appiumby import AppiumBy
+import os
+
 class AppInit:
+    CONST_WAIT_TIME = 1 # in sec
     capabilities = dict (
         platformName='Android',
         automationName='uiautomator2',
@@ -9,22 +12,33 @@ class AppInit:
         language='zh',
         locale='CN',
         localeScript='Hans',
-        # app='\\testApk\\news_article.apk'，
+        autoGrantPermissions = True,
         noReset = True
     )
     appium_server_url = 'http://localhost:4723'
-    def __init__(self, apk_path):
-        self.capabilities['app'] = apk_path
+    def __init__(self, raw_data):
+        self.apk_path = raw_data['apk_path']
+        self.root_path = raw_data['root_path']
+        self.screen_record_path = raw_data['screen_record_path']
+        full_path = os.path.join(os.getcwd(), self.apk_path)
+        self.capabilities['app'] = full_path
         pass
 
     def first_run(self, steps_info):
         self.driver = webdriver.Remote(self.appium_server_url,
                                   options=UiAutomator2Options().load_capabilities(self.capabilities))
+        self.driver.implicitly_wait(self.CONST_WAIT_TIME)
         for info in steps_info:
+            print (info)
             self.find_and_click(info)
+        self.record_status("complete_first_run")
+        
+    def record_status(self, section_name):
+        file_name = section_name + ".png"
+        file_name = os.path.join(self.screen_record_path, file_name)
+        self.driver.get_screenshot_as_file(file_name)
         
     def find_and_click(self, info):
-        self.driver.implicitly_wait(1000)
         items = self.driver.find_elements(by=AppiumBy.XPATH, value=info['pattern'])
         if len(items) > 0:
             items[0].click()
--- start.py
+++ start.py
@@ -6,10 +6,8 @@ def main():
     test_file = os.path.join(os.getcwd(),'testApk/news_article/config.json')
     with open(test_file, 'r', encoding='utf-8') as file:
         raw = json.load(file)
-    print(raw['apk_root'])
-    path = os.path.join(os.getcwd(), 'testApk/news_article', raw['apk_root'])
-    print (path)
-    AppInit(path).first_run(steps_info=raw['launch_step'])
+    print(raw['apk_path'])
+    AppInit(raw).first_run(steps_info=raw['launch_step'])
 
 if __name__ == "__main__" :
     main()
